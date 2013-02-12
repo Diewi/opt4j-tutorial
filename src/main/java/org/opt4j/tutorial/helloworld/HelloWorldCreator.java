9b3c5516e90b3470629a357ab291a8311b5cc7ba
martin lukasiewycz <martin.lukasiewycz@CE006-4.TUMCREATE.LOCAL>
2013-02-12 23:28:10 +0800

    Add ohloh facts to the home page

---
+++ src/main/java/org/opt4j/tutorial/helloworld/HelloWorldCreator.java
@@ -0,0 +1,21 @@
+package org.opt4j.tutorial.helloworld;
+
+import java.util.Random;
+
+import org.opt4j.core.problem.Creator;
+import org.opt4j.genotype.SelectGenotype;
+
+public class HelloWorldCreator implements Creator<SelectGenotype<Character>> {
+
+	Character[] ALPHABET = { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R',
+			'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', ' ' };
+	Random random = new Random();
+
+	@Override
+	public SelectGenotype<Character> create() {
+		SelectGenotype<Character> genotype = new SelectGenotype<Character>(ALPHABET);
+		genotype.init(random, 11);
+		return genotype;
+	}
+
+}
---
+++ src/main/java/org/opt4j/tutorial/helloworld/HelloWorldEvaluator.java
@@ -0,0 +1,23 @@
+package org.opt4j.tutorial.helloworld;
+
+import org.opt4j.core.Objective;
+import org.opt4j.core.Objective.Sign;
+import org.opt4j.core.Objectives;
+import org.opt4j.core.problem.Evaluator;
+
+public class HelloWorldEvaluator implements Evaluator<String> {
+
+	Objective objective = new Objective("objective", Sign.MAX);
+
+	@Override
+	public Objectives evaluate(String phenotype) {
+		int value = 0;
+		for (int i = 0; i < phenotype.length(); i++) {
+			value += (phenotype.charAt(i) == "HELLO WORLD".charAt(i)) ? 1 : 0;
+		}
+
+		Objectives objectives = new Objectives();
+		objectives.add(objective, value);
+		return objectives;
+	}
+}
